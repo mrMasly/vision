741c4bf3f2b54d4beb8eb0b1a6b797168ee8bead
mrmasly <mrmasly@gmail.com>
2017-03-18 20:55:48 +1000

    при смене url проверять доступ

--- app/Navigation.vue
+++ app/Navigation.vue
@@ -10,6 +10,8 @@ md-bottom-bar
 
 <script lang="coffee">
 
+import _ from 'lodash'
+
 component =
   name: 'Sidenav'
   data: ->
@@ -21,7 +23,17 @@ component =
     for tab in @$store.state.tabs
       Meteor.registerModule tab.module
       @tabs.push tab if tab.module.access()
+      
     @$router.beforeEach (to, from, next) =>
+
+      # проверям доступность нового адреаса
+      nextRoute = _.find @$store.state.routes, name: to.name
+      module = nextRoute?.component?.module
+      if module? and module.access?
+        if module.access() is no
+          # дуступ закрыт
+          return @$router.push path: from.path
+
       @$store.commit 'selectTab', to
       @$store.commit 'rememberTab', from
       do next
