73b8197434768742e5e09d9576568c3d3ab27ca3
mrmasly <mrmasly@gmail.com>
2017-03-19 11:54:43 +1000

    подготовил и опублиоквал в atmosphere

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'mrmasly:vision',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '0.1.0',
+  summary: 'Vision app',
+  git: 'https://github.com/mrMasly/vision',
   documentation: 'README.md'
 });
 
@@ -77,11 +73,4 @@ Package.onUse(function(api) {
 
 
 
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('mrmasly:vision');
-  api.mainModule('vision-tests.js');
 });
