d82ba8455f90069455701efc4ccb909cc9792ad0
mrmasly <mrmasly@gmail.com>
2017-04-10 12:20:34 +1000

    Vision-access - move to package

--- main/server.coffee
+++
@@ -1,1 +0,0 @@
-import '../app/access/server/index.coffee'
---
+++ packages/access/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'mrmasly:vision-access',
+  version: '0.0.1',
+  summary: 'Vision access',
+  git: 'https://github.com/mrMasly/vision',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.4.3.1');
+
+  api.use('mrmasly:vision');
+  api.use('akryum:npm-check@0.0.2');
+  api.use('coffeescript@1.12.3_1');
+  api.use('ecmascript@0.6.3');
+
+  api.use('accounts-base@1.2.15');
+  api.use('accounts-password@1.3.4');
+
+  api.use('mrmasly:vue@0.1.4');
+  api.use('mrmasly:files@0.0.3');
+
+  api.addFiles('Access.vue', 'client');
+  api.addFiles('server/index.coffee', 'server');
+
+});
