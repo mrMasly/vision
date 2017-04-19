e9ab1fbb82a3972c866815ee2436631dfe16e3d7
mrmasly <mrmasly@gmail.com>
2017-04-19 12:41:12 +1000

    add moment and numeral filters

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mrmasly:vision',
-  version: '0.3.2',
+  version: '0.3.4',
   summary: 'Vision app',
   git: 'https://github.com/mrMasly/vision',
   documentation: 'README.md'
