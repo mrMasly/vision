2b584428af7fd7a5679a2584672488ed3912a275
mrMasly <mrmasly@gmail.com>
2017-05-06 16:52:46 +1000

    update mrmasly:vue 0.2.2

--- package.js
+++ package.js
@@ -40,7 +40,7 @@ Package.onUse(function(api) {
   api.use('accounts-base@1.2.15');
   api.use('accounts-password@1.3.4');
 
-  api.use('mrmasly:vue@0.2.1');
+  api.use('mrmasly:vue@0.2.2');
   api.use('mrmasly:files@0.0.3');
 
   api.addAssets([
