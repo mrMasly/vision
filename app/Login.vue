c65fd2008ab9d4b9d40e3290c653408e44e1a944
mrMasly <mrmasly@gmail.com>
2017-05-15 12:36:56 +1000

    Logon - add enter event

--- app/Login.vue
+++ app/Login.vue
@@ -6,10 +6,11 @@
     v-card-content
       v-input-container
         label Имя
-        v-input(v-model="username")
+        v-input(v-model="username" @keydown.native.enter="login")
       v-input-container(has-password, :class="{ 'v-input-invalid': error }")
         label Пароль
-        v-input(type="password" v-model="password" @change="error=''")
+        v-input(type="password" v-model="password" @change="error=''"
+          @keydown.native.enter="login")
         .v-error(v-if="error") {{error}}
     v-card-actions
       v-button.v-primary(@click.native="login") Войти
