5349fd517a5524cb62ba7eb4ad80d399375d4a3f
mrMasly <mrmasly@gmail.com>
2017-06-20 13:15:11 +1000

    vCollapse - created

---
+++ components/v-collapse/index.coffee
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import vCollapse from './vCollapse.vue'
+
+Vue.component 'v-collapse', vCollapse
---
+++ components/v-collapse/vCollapse.vue
@@ -0,0 +1,29 @@
+<template lang="jade">
+.v-collapse
+  .l-relative
+    v-ripple
+    v-subheader.v-primary.toggle(
+    @click.native="data[prop]=!data[prop]")
+      .l-row.l-start-center
+        v-icon(v-if="data[prop]") keyboard_arrow_down
+        v-icon(v-else) chevron_right
+        .l-flex {{label}}
+  slot(v-if="data[prop]")
+</template>
+
+<script lang="coffee">
+component =
+  name: 'Toggle'
+  props:
+    data:
+      type: Object
+      required: yes
+    label:
+      type: String
+      required: yes
+    prop:
+      type: String
+      default: 'toggle'
+
+return component
+</script>
