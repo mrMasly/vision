2f2028bc592507dc88d1b586734198103deac84a
mrMasly <mrmasly@gmail.com>
2017-07-17 09:25:45 +1000

    vLoading - created

---
+++ components/v-loading/index.coffee
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import vLoading from './vLoading.vue'
+
+Vue.component 'v-loading', vLoading
---
+++ components/v-loading/vLoading.vue
@@ -0,0 +1,33 @@
+<template lang="jade">
+transition(name="vloading")
+  slot.v-loading-child(v-if="!loading")
+  .v-loading.v-loading-child(v-else)
+    svg.circular(viewbox='25 25 50 50')
+      circle.path(cx='50', cy='50', r='20', fill='none', stroke-width='2', stroke-miterlimit='10')
+</template>
+
+<script lang="coffee">
+import _ from 'lodash'
+component =
+  name: 'vLoading'
+  watch:
+    value: ->
+      do @check
+  props:
+    value: required: no
+    color:
+      type: String
+      default: '#fff'
+  data: ->
+    loading: yes
+  created: ->
+    @check = _.throttle @check, 500
+    do @check
+  methods:
+    check: -> @loading = !@value
+  computed:
+    styles: ->
+      backgroundColor: @color
+
+return component
+</script>
