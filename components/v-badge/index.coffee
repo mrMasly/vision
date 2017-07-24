a9fd387c137c957eda152c75033b3098d51462fb
mrMasly <mrmasly@gmail.com>
2017-07-24 15:56:44 +1000

    vBadge - created

---
+++ components/v-badge/index.coffee
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import vBadge from './vBadge.vue'
+
+Vue.component 'v-badge', vBadge
---
+++ components/v-badge/vBadge.vue
@@ -0,0 +1,15 @@
+<template lang="jade">
+.v-badge(v-if="value") {{ (value>99) ? '99+' : value }}
+</template>
+
+<script lang="coffee">
+component =
+  name: 'vBadge'
+  props:
+    value: Number
+
+return component
+</script>
+
+<style lang="stylus" scoped>
+</style>
