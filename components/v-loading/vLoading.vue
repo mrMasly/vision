6c870f8f82525f1c85d7119c53f8e8b3fc2e32b3
mrMasly <mrmasly@gmail.com>
2017-07-26 13:23:22 +1000

    vLoading: перенес стили в компонент, чтобы нормально отображалось

--- components/v-loading/vLoading.vue
+++ components/v-loading/vLoading.vue
@@ -1,5 +1,5 @@
 <template lang="jade">
-transition(name="vloading")
+transition(name="fade")
   slot.v-loading-child(v-if="!loading")
   .v-loading.v-loading-child(v-else)
     svg.circular(viewbox='25 25 50 50')
@@ -24,12 +24,89 @@ component =
     @check = _.throttle @check, 500
     do @check
   methods:
-    check: ->
-      @loading = !@value
-      if @value then @$emit 'load'
+    check: -> @loading = !@value
   computed:
     styles: ->
       backgroundColor: @color
 
+
 return component
 </script>
+
+<style lang="stylus" scoped>
+.fade-enter-active
+.fade-leave-active
+  transition: opacity .5s
+.fade-enter
+.fade-leave-to
+  opacity  0
+
+.v-loading-child
+  position absolute
+  top 0
+  left 0
+  width 100%
+  height 100%
+.v-loading
+  display flex
+  align-content center
+  align-items center
+  justify-content center
+
+.loader {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  left: 50%;
+  top: 20%;
+}
+
+.circular {
+  animation: rotate 2s linear infinite;
+  height: 100px;
+  position: relative;
+  width: 100px;
+}
+
+.path {
+  stroke-dasharray: 1,200;
+  stroke-dashoffset: 0;
+  animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out infinite;
+  stroke-linecap: round;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1,200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 89,200;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 89,200;
+    stroke-dashoffset: -124;
+  }
+}
+@keyframes color {
+  100%, 0% {
+    stroke: #d62d20;
+  }
+  40% {
+    stroke: #0057e7;
+  }
+  66% {
+    stroke: #008744;
+  }
+  80%, 90% {
+    stroke: #ffa700;
+  }
+}
+
+</style>
