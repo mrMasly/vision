c5ce9b0d3bc71b2c337d8310230053e873a36f3c
mrMasly <mrmasly@gmail.com>
2017-08-03 10:48:44 +1000

    Toolbar - переделал отображение кнопок на малых экранах

--- app/Toolbar.vue
+++ app/Toolbar.vue
@@ -1,8 +1,12 @@
 <template lang="jade">
 v-toolbar.v-shadow-2
-  h1.v-title.l-flex {{title}}
-  .components.l-row
+  h1.v-title.l-flex(v-if="$layout.gxs || !show") {{title}}
+  .components.l-row(v-if="$layout.gxs")
     component(:is="one" v-for="one in components", :key="one.module.id")
+  .absolute.l-row.l-end-center(v-else)
+    component(:is="one" v-for="one in components", :key="one.module.id" v-if="show")
+    v-button.v-icon-button(@click.native="toggle")
+      v-icon menu
 </template>
 
 <script lang="coffee">
@@ -10,6 +14,7 @@ import _ from 'lodash'
 component =
   name: 'Toolbar'
   data: ->
+  
     components = _.filter Meteor.Components, (one) ->
       return no unless one.nav
       return no unless one.module.access()
@@ -20,7 +25,7 @@ component =
 
     title: null
     components: components
-    
+    show: no
   created: ->
     @getTitle @$route
     @$router.beforeEach (to, from, next) =>
@@ -31,6 +36,8 @@ component =
     getTitle: (route) ->
       route = _.find @$router.options.routes, name: route.name
       @title = route?.title ? null
+    toggle: ->
+      @show = !@show
 
 return component
 </script>
@@ -38,4 +45,11 @@ return component
 <style lang="stylus" scoped>
 .v-toolbar
   z-index 2
+.absolute
+  position absolute
+  // background-color #2196f3
+  width 100%
+  height 100%
+  right 0
+  top 0
 </style>
